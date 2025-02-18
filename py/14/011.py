1465fd6893553ef47ccf0eaede38b93c70a8a107
zvinorez <cebepeh@gmail.com>
2025-02-18 17:56:42 +0400

    hi

---
+++ py/14/011.py
@@ -0,0 +1,10 @@
+# №244 kompege
+
+for x in range(100):
+    try:
+        if int("21", x) * int("13", x) == int("313", x):
+            print(x)
+    except:
+        ...
+
+# Ответ: 6
---
+++ py/14/012.py
@@ -0,0 +1,11 @@
+# №4702 kompege
+
+for x in range(15):
+    try:
+        n = int(f"123{x}5", 15) + int(f"1{x}233", 15)
+        if n % 14 == 0:
+            print(n//14)
+    except:
+        ...
+
+# Ответ: 8767
