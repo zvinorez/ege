51ad18d4991b880e119502620b7475078f1062f8
zvinorez <cebepeh@gmail.com>
2025-03-03 18:27:44 +0400

    hi

--- py/14/012.py
+++ py/14/012.py
@@ -4,7 +4,7 @@ for x in range(15):
     try:
         n = int(f"123{x}5", 15) + int(f"1{x}233", 15)
         if n % 14 == 0:
-            print(n//14)
+            print(n // 14)
     except:
         ...
 
