d019c2fefaa98ffb6243b79c5e6c4b239dd98ca4
zvinorez <cebepeh@gmail.com>
2025-03-04 15:48:31 +0400

    hi

--- py/13/008.py
+++ py/13/008.py
@@ -4,10 +4,12 @@ from ipaddress import *
 c = 0
 
 net = ip_network("192.168.240.0/255.255.255.0")
-a = list(net.hosts())
-for ip in a:
+a = list(net.hosts()) # это лишнее, можно было просто for ip in net:
+for ip in a:          # но я чет тупанул
     b = f"{ip:b}"
     if b.count("0") == b.count("1"):
         c += 1
 print(c)
+
+
 # Ответ: 8
